4c86b0548d674de54d44b32aaf9f224395378f30
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-02 21:58:16 -0600

    Initial Commit

---
+++ BarcodeScannerPractice/BarcodeScannerPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerPracticeApp.swift
+//  BarcodeScannerPractice
+//
+//  Created by Mitch Andrade on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
