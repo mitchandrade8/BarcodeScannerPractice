4ac4579969280056f18e4c2220ae0347bef15d96
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-04 00:43:20 -0600

    --Updating: imported AVFoundation and using ScannerVC to handle the barcode logic with camera

--- BarcodeScannerPractice/ScannerVC.swift
+++ BarcodeScannerPractice/ScannerVC.swift
@@ -5,4 +5,23 @@
 //  Created by Mitch Andrade on 10/4/23.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+
+protocol ScannerVCDelegate: class {
+    func didFind(barcode: String)
+}
+
+final class ScannerVC: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerVCDelegate!
+    
+    init(scannerDelegate: ScannerVCDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented")}
+}
