4d304fe71a7bd128c9649f4cd9a2b00e98379b8c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-04 01:13:10 -0600

    --Stopping: can not retain any knowledge right now... will take a nap and comeback later to finish

--- BarcodeScannerPractice/ScannerVC.swift
+++ BarcodeScannerPractice/ScannerVC.swift
@@ -24,4 +24,42 @@ final class ScannerVC: UIViewController {
     }
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented")}
+    
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13]
+        } else {
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill
+    }
+}
+
+extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
+    
 }
