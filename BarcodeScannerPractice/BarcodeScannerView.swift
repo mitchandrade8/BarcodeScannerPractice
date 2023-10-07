94eef8b43082d19afb1d1ebd4d85b8137eaad764
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-07 14:46:59 -0600

    -- Progress: created new structs for handling errors/ alertItems, refactoring

--- BarcodeScannerPractice/BarcodeScannerView.swift
+++ BarcodeScannerPractice/BarcodeScannerView.swift
@@ -7,9 +7,30 @@
 
 import SwiftUI
 
+struct AlertItem {
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    
+    static let invalidDeviceInput = AlertItem(
+        title: "Invalid Device Input",
+        message: "Something is wrong with the camera, We are unable to capture the input.",
+        dismissButton: .default(Text("Ok")))
+    
+    static let invalidScannedType = AlertItem(
+        title: "Invalid Scan Type",
+        message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.",
+        dismissButton: .default(Text("Ok")))
+    
+}
+
 struct BarcodeScannerView: View {
     
     @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         NavigationView {
@@ -26,6 +47,7 @@ struct BarcodeScannerView: View {
                     .font(.system(size: 36, weight: .semibold, design: .default))
                     .foregroundColor(scannedCode.isEmpty ? .red : .green)
                     .padding()
+ 
             }
             .navigationTitle("Barcode Scanner")
         }
